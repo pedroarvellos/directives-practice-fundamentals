0940bf2f3e84cc88c7383282392f383d40841523
SANTOS ARVELLOS Pedro Gonçalves <pgsantosarvellos@europe.corp.altran.com>
2020-02-05 11:46:38 +0000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives';
+  courses = [1, 2];
+  viewMode = 'map'
+  coursesTwo = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' }
+  ]
+  coursesThree = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' }
+  ]
+
+  coursesFour;
+
+  onAdd() {
+    this.coursesThree.push({ id: 4, name: 'course 4' })
+  }
+
+  onRemove(course) {
+    let index = this.coursesThree.indexOf(course)
+    this.coursesThree.splice(index, 1)
+  }
+
+  // In Angular a pass the reference of the original attribute of the object,
+  // so, if I change it, it changes the original.
+  onChange(course) {
+    course.name = 'Updated'
+    console.log(this.coursesThree)
+  }
+
+  loadCourses() {
+    this.coursesFour = [
+      { id: 1, name: 'course 1' },
+      { id: 2, name: 'course 2' },
+      { id: 3, name: 'course 3' }
+    ]
+  }
+
+  trackCourse(index, course) {
+    console.log(index)
+    console.log(course)
+    return course ? course.id : undefined
+  }
 }
